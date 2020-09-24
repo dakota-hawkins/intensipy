835a31e37ada6cc90da86fffcde9f48c99139265
dakota-hawkins <dakotahawkins0110@gmail.com>
2020-09-24 11:38:49 -0400

    package files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='intensipy',
+      version='0.1',
+      description="Normalize intensity values in 3D images.",
+      url='https://github.com/dakota-hawkins/intensipy',
+      author='Dakota Y. Hawkins',
+      author_email='dyh0110@bu.edu',
+      license='BSD',
+      packages=['intensipy'],
+      zip_safe=False)
