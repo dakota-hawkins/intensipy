da3b99cbd8de8bd542e94f7f1bac006dbdaa102f
dakota-hawkins <dakotahawkins0110@gmail.com>
2020-09-29 13:56:20 -0400

    default contrast stretch now skimage

--- intensipy/models.py
+++ intensipy/models.py
@@ -26,6 +26,7 @@ class Intensify:
     def __init__(self, xy_norm=True, z_norm=True,
                  t=None, dy=None, dx=None, n_quantiles=10000,
                  smooth_quartiles=True, keep_original_scale=True,
+                 stretch_method='skimage',
                  bits=12):
         """
         Normalizes 3D image stacks using Intensify3D.
@@ -66,6 +67,7 @@ class Intensify:
         self.n_quantiles = n_quantiles
         self.smooth_quartiles = smooth_quartiles
         self.keep_original_scale = keep_original_scale
+        self.stretch_method = stretch_method
         self.bits = bits
 
 # ----------------------------- Properties -------------------------------------
@@ -77,7 +79,7 @@ class Intensify:
     @xy_norm.setter
     def xy_norm(self, value):
         if not isinstance(value, bool):
-            raise ValueError("Expected boolean value for `xy_norm`.")
+            raise TypeError("Expected boolean value for `xy_norm`.")
         self.xy_norm_ = value
 
     @property
@@ -88,7 +90,7 @@ class Intensify:
     @z_norm.setter
     def z_norm(self, value):
         if not isinstance(value, bool):
-            raise ValueError("Expected boolean value for `z_norm`.")
+            raise TypeError("Expected boolean value for `z_norm`.")
         self.z_norm_ = value
     
     @property
@@ -99,7 +101,7 @@ class Intensify:
     @dy.setter
     def dy(self, value):
         if not Intensify.check_numeric(value) and value is not None:
-            raise ValueError(f"`dy` should be numeric. Recieved {type(value)}.")
+            raise TypeError(f"`dy` should be numeric. Recieved {type(value)}.")
         if value is None:
             value = 29
         self.dy_ = value
@@ -112,7 +114,7 @@ class Intensify:
     @dx.setter
     def dx(self, value):
         if not Intensify.check_numeric(value) and value is not None:
-            raise ValueError(f"`dx` should be numeric. Recieved {type(value)}.")
+            raise TypeError(f"`dx` should be numeric. Recieved {type(value)}.")
         if value is None:
             value = 29
         self.dx_ = value
@@ -125,7 +127,7 @@ class Intensify:
     @n_quantiles.setter
     def n_quantiles(self, value):
         if not isinstance(value, (int, np.integer)):
-            raise ValueError("Expected integer values for `n_quantile`."\
+            raise TypeError("Expected integer values for `n_quantile`."\
                              f" Received {type(value)}.")
         self.n_quantiles_ = value
         
@@ -137,7 +139,7 @@ class Intensify:
     @smooth_quartiles.setter
     def smooth_quartiles(self, value):
         if not isinstance(value, bool):
-            raise ValueError("Expected boolean value for `smooth_quartiles`."\
+            raise TypeError("Expected boolean value for `smooth_quartiles`."\
                              f" Received {type(value)}.")
         self.smooth_quartiles_ = value 
 
@@ -149,9 +151,23 @@ class Intensify:
     @keep_original_scale.setter
     def keep_original_scale(self, value):
         if not isinstance(value, bool):
-            raise ValueError("Expected boolean value for `keep_original_scale`."\
+            raise TypeError("Expected boolean value for `keep_original_scale`."\
                              f" Received {type(value)}.")
         self.keep_original_scale_ = value
+
+    @property
+    def stretch_method(self):
+        """Method to perform constrast stretching."""
+        return self.stretch_method_
+
+    @stretch_method.setter
+    def stretch_method(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Expected string for `stretch_method`.")
+        if value.lower() not in ['skimage', 'intensify3d']:
+            raise ValueError(f"{value} not a valid constrast-stretch method."\
+                              "Supported methods are 'skimage' or 'intensify3d.")
+        self.stretch_method_ = value.lower()
 
     @property
     def bits(self):
@@ -160,7 +176,9 @@ class Intensify:
     
     @bits.setter
     def bits(self, value):
-        self.bits_ = value
+        if float(value) != int(value):
+            raise TypeError("Expected integer value for `bits`.")
+        self.bits_ = int(value)
         
 # ------------------------------ Class Functions -------------------------------
     def normalize(self, z_stack, t=None, ref_idx=None, verbose=True):
@@ -198,7 +216,8 @@ class Intensify:
             thresholds = np.array([np.percentile(x.flatten(), cutoff)\
                                for x in z_stack])
         else:
-            thresholds = np.array([filters.threshold_otsu(x) for x in z_stack])
+            thresholds = np.array([filters.threshold_otsu(x)\
+                                   for x in z_stack])
 
         # create empty z-stack for normalized data
         normed = np.zeros_like(z_stack, dtype='float')
@@ -206,9 +225,10 @@ class Intensify:
         semi_quantiles = np.zeros((z_stack.shape[0], self.n_quantiles))
         p_quantiles = np.arange(0, 100, 100 / self.n_quantiles)
         # normalize along xy axis for each image.
-        if verbose:
-            print("Normalizing intensity values across xy-dimensions.")
+
         if self.xy_norm_:
+            if verbose:
+                print("Normalizing intensity values across xy-dimensions.")
             for i in Intensify.get_iterator(z_stack.shape[0], verbose):
                 normed[i], thresholds[i] = self.xy_normalize(np.array(z_stack[i]),
                                                              thresholds[i])
@@ -223,15 +243,14 @@ class Intensify:
         agg_quantiles = np.percentile(semi_quantiles, 98, axis=0)
         semi_median = agg_quantiles[self.n_quantiles // 2]
         semi_max = agg_quantiles[-1]
-        transform = interpolate.interp1d(np.arange(self.n_quantiles),
-                                         agg_quantiles)
-
+        self.transform_ = interpolate.interp1d(np.arange(self.n_quantiles),
+                                               agg_quantiles)
+  
         if self.z_norm:
             if verbose:
                 print("Normalizing intensity values across z-dimension.")
             for i in Intensify.get_iterator(z_stack.shape[0], verbose):
                 normed[i] = self.z_normalize(normed[i], thresholds[i],
-                                             transform,
                                              semi_median, semi_max)
         return self.standardize_output(normed)
 
@@ -264,7 +283,7 @@ class Intensify:
         norm_xy = img / mask
         # standardize to old values
         norm_xy /= (np.median(norm_xy) / np.median(img))
-        new_t = norm_xy[np.logical_and(fg, norm_xy > 0)].min()
+        new_t = norm_xy[fg].min()
         return (norm_xy, new_t)
 
     def smooth_background(self, img, selected, method='savitzky-galore'):
@@ -306,7 +325,7 @@ class Intensify:
             raise ValueError(f"Unsupported smoothing operation: {method}.")
         return mask
 
-    def z_normalize(self, img, t, transform, semi_median, semi_max):
+    def z_normalize(self, img, t, semi_median, semi_max):
         """
         Normalize an image along the Z dimension using semi-quantile normalizaiton. 
 
@@ -331,12 +350,11 @@ class Intensify:
         (lower, upper) = np.percentile(img[img > 0], [25, 99])
         fg = img >= t
         # perform constrast stretching on foreground pixels
-        img[fg] = Intensify.constrast_stretch(img[fg], lower, upper,
+        img[fg] = self.constrast_stretch(img[fg], lower, upper,
                                               semi_median, semi_max)
         # quantile normalization on background pixels
-        img[~fg] = Intensify.quantile_normalization(img[~fg],
-                                                    self.n_quantiles,
-                                                    transform)
+        img[~fg] = self.quantile_normalization(img[~fg],
+                                               self.n_quantiles)
         # re-scale foreground intensities
         img[fg] *= np.max(img[~fg]) / np.min(img[fg])
         # quantile normalization produces a decent amount of static:
@@ -358,6 +376,59 @@ class Intensify:
         else:
             out = exposure.rescale_intensity(img)
         return out
+
+    def constrast_stretch(self, pixels, lower, upper, semi_median, semi_max):
+        """
+        Constrast stretch pixels following methods outline in original paper.
+        
+        Parameters
+        ----------
+        pixels: np.ndarray
+        lower: float
+            Intensity value corresponding to the 1st quartile of `pixels`.
+        upper: float
+            Intensity value corresponding to the 98th percentile of `pixels`.
+        semi_median: float
+            Intensity value corresponding to the median of semi-quantiles.
+        semi_max: float
+            Intensity value corresponding to the maximum of semi-quantiles.
+
+        Return
+        ------
+        np.ndarray
+            Adjusted pixels
+        """
+        if self.stretch_method == 'intensify3d':
+            out = (pixels - lower)\
+                * ((semi_max - semi_median) / (upper - lower))\
+                + semi_median
+        else:
+            out = exposure.rescale_intensity(pixels,
+                                             out_range=(semi_median, semi_max))
+        return out
+
+    def quantile_normalization(self, pixels, n_quantiles):
+        """
+        Perform quantile normalization.
+        
+        Parameters
+        ----------
+
+        pixels : np.ndarray
+            Values to normalize.
+        n_quantiles : int
+            Number of quantiles to compute.
+        transform : callable
+            Callable to transform quantile position to intensity value.
+
+        Returns
+        -------
+        np.ndarray
+            Normalized pixel intensities.
+        """
+        # transform pixel ranks to aggregate quantile space
+        values = self.transform_(np.linspace(0, n_quantiles - 1, pixels.size))
+        return values[np.argsort(pixels)]
 
 # ---------------------------- Private Helper Functions ------------------------  
     def __check_stack(self, z_stack):
@@ -424,57 +495,6 @@ class Intensify:
                                              intensities < p2)]
         ecdf = ECDF(samples)
         return ecdf.x[np.searchsorted(ecdf.y, np.random.uniform(size=n))] 
-
-    @staticmethod
-    def quantile_normalization(pixels, n_quantiles, transform):
-        """
-        Perform quantile normalization.
-        
-        Parameters
-        ----------
-
-        pixels : np.ndarray
-            Values to normalize.
-        n_quantiles : int
-            Number of quantiles to compute.
-        transform : callable
-            Callable to transform quantile position to intensity value.
-
-        Returns
-        -------
-        np.ndarray
-            Normalized pixel intensities.
-        """
-        # transform pixel ranks to aggregate quantile space
-        values = transform(np.linspace(0, n_quantiles - 1, pixels.size))
-        return values[np.argsort(pixels)]
-
-    @staticmethod
-    def constrast_stretch(pixels, lower, upper, semi_median, semi_max):
-        """
-        Constrast stretch pixels following methods outline in original paper.
-        
-        Parameters
-        ----------
-        pixels: np.ndarray
-        lower: float
-            Intensity value corresponding to the 1st quartile of `pixels`.
-        upper: float
-            Intensity value corresponding to the 98th percentile of `pixels`.
-        semi_median: float
-            Intensity value corresponding to the median of semi-quantiles.
-        semi_max: float
-            Intensity value corresponding to the maximum of semi-quantiles.
-
-        Return
-        ------
-        np.ndarray
-            Adjusted pixels
-        """
-        out = (pixels - lower)\
-            * ((semi_max - semi_median) / (upper - lower))\
-            + semi_median
-        return out
 
     @staticmethod
     def savitzky_galoy(img, dy, dx, k):
